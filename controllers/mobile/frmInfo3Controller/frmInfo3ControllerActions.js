980cc2de3e88c947a877f0086b591640682a8d86
KITSPL\KH2287 <anand.nettikadan@kony.com>
2019-10-19 23:09:36 +0530

    re committing the forms - frm Info1 , frm Info2

---
+++ controllers/mobile/frmInfo3Controller/frmInfo3ControllerActions.js
@@ -0,0 +1,9 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onClick defined for flxInnerFooter1 **/
+    AS_FlexContainer_bb48e93ed92443be989a6b890437c246: function AS_FlexContainer_bb48e93ed92443be989a6b890437c246(eventobject) {
+        var self = this;
+    }
+});

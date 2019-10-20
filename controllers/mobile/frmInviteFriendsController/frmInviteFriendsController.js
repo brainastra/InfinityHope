1be61b54cd90d187aae1adda9351f07e53103a67
shekhar_kony <shekhar.suman@kony.com>
2019-10-20 14:06:18 +0530

    updated invite friends

--- controllers/mobile/frmInviteFriendsController/frmInviteFriendsController.js
+++ controllers/mobile/frmInviteFriendsController/frmInviteFriendsController.js
@@ -1,8 +1,8 @@
 define({ 
 
  sendInvite: function(){
-    var x = new kony.mvc.Navigation("frmHopeDashboard");
-    x.navigate({});
+    var x = new kony.mvc.Navigation("frmHopeDashboard");    
+    var obj={amount:0};
+    x.navigate(obj);
  }
-
  });

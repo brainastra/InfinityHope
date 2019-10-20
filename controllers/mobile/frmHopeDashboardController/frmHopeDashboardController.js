312ecc9e344893976c941feb42d0ba78b51f575f
shekhar_kony <shekhar.suman@kony.com>
2019-10-20 13:54:48 +0530

    donut changes

--- controllers/mobile/frmHopeDashboardController/frmHopeDashboardController.js
+++ controllers/mobile/frmHopeDashboardController/frmHopeDashboardController.js
@@ -1,5 +1,60 @@
 define({ 
+  preShow: function(){
 
- //Type your controller code here 
+  },
 
- });
+  onNavigate: function(){
+    var amount;
+
+    if(arguments[0]!=null || arguments[0]!=undefined){
+      amount =  arguments[0].amount;
+
+    }
+    else{ 
+      amount = 90;
+    }
+
+    var chartDataToSet;
+    if( amount >= 100){
+      this.view.imgTarget.isVisible = true;
+      this.view.lblTragetComplete.isVisible = true;
+      this.view.lblTarget.text = "$100/$100";
+      this.view.flxAlert.isVisible = false;
+      this.view.flxGraphs.isVisible = true;
+      chartDataToSet = [
+        {"colorCode": "#1b9ed9", "label": "Jack", "value": 40}, 
+        {"colorCode": "#e8672b", "label": "Adam", "value": 30}, 
+        {"colorCode": "#76c044", "label": "Alexa", "value": 30}
+      ];
+    }
+    else if(amount == 0){
+      this.view.imgTarget.isVisible = false;
+      this.view.lblTragetComplete.isVisible = false;
+      this.view.lblTarget.isVisible = false;
+      this.view.flxAlert.isVisible = true;
+      this.view.flxGraphs.isVisible = false;
+      this.view.lblTargerHeader.isVisible = false;
+    }
+    else
+    {
+      this.view.imgTarget.isVisible = false;
+      this.view.lblTragetComplete.isVisible = false;
+      this.view.lblTarget.text = "$22.55/$100";
+      this.view.flxAlert.isVisible = false;
+      this.view.flxGraphs.isVisible = true;
+      chartDataToSet = [
+        {"colorCode": "#1b9ed9", "label": "Jack", "value": 10.50}, 
+        {"colorCode": "#e8672b", "label": "Adam", "value": 7.50}, 
+        {"colorCode": "#76c044", "label": "Alexa", "value": 4.55}
+      ];
+
+    }
+    this.view.donutchart.chartData = {
+      "data": chartDataToSet
+    };
+    this.view.forceLayout();
+
+  },
+
+});
+
